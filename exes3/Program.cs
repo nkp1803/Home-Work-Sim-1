4338b92fedf92ade0abd3dd36803b84906358045
konstantin <nkp.yalta@mail.ru>
2022-06-13 14:54:41 +0300

    add exes 3

---
+++ exes3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+Console.Write("Введите первое число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+    if (numberInt%2==0)
+    {
+        Console.WriteLine("Число является четным  ");
+    }
+    else
+    {
+        Console.WriteLine("Число является не четным ");
+    }
