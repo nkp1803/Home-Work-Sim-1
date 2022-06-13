9d568e39cc37be5afdbd00e66e6ef7a7b599715c
konstantin <nkp.yalta@mail.ru>
2022-06-13 15:38:10 +0300

    add exes 4

---
+++ exes4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+Console.Write("Введите первое число: ");
+string number = Console.ReadLine();
+int numberIntN = int.Parse(number);
+int index = 1;
+while (index <= numberIntN )
+{
+   if (index%2==0)
+   {
+        Console.WriteLine(index);
+   }
+   index++;
+}
+Console.WriteLine("End");
